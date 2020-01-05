3c38cfaa92123ab28ed1047f59b4f83b54ae564d
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-01-05 03:08:38 +0100

    add tool exploitation from gtfobins

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from mmpy_bot.bot import Bot
+import pypandoc
 from mmpy_bot.bot import listen_to
 from mmpy_bot.bot import respond_to
 import os
@@ -7,11 +8,19 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-@listen_to('!bot help', re.IGNORECASE)
+@listen_to('!gimme help', re.IGNORECASE)
 def help(message):
-    message.reply('I don\'t know any commands yet :(')
+    commands  =[
+            "`!gimme shell <language>` Give me a reverse-shell in <language>",
+            "`!gimme <cve-id>` Give me info about CVE-<id>",
+            "`!gimme tool <toolname>` Give me misuse of <toolname>"]
 
-@listen_to('!shell (.*)', re.IGNORECASE)
+    r = "I know the following commands:\n"
+    for item in commands:
+        r += "\n- " + item
+    message.send(r)
+
+@listen_to('!gimme shell (.*)', re.IGNORECASE)
 def revshell(message, lang):
     shells = os.listdir('./shells')
     if lang in shells:
@@ -26,7 +35,7 @@ def revshell(message, lang):
         message.send("Supported languages are:")
         message.send(', '.join(shells))
 
-@listen_to('!cve([0-9-]*)', re.IGNORECASE)
+@listen_to('!gimme cve([0-9-]*)', re.IGNORECASE)
 def cve(message,id):
 
     link = 'http://cve.circl.lu/cve/CVE'+id
@@ -35,6 +44,27 @@ def cve(message,id):
 
     for item in soup.select("#cveInfo > tbody > tr:nth-child(2) > td.info"):
         message.send('```pre\n'+item.get_text()+'\n```')
+
+
+@listen_to('!gimme tool (.*)', re.IGNORECASE)
+def tool(message,id):
+
+    link = 'https://gtfobins.github.io/gtfobins/'+id
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, "lxml")
+
+    for div in soup.find_all("ul", {'class':'function-list'}):
+        div.decompose()
+
+    for div in soup.find_all("h1"):
+        div.decompose()
+
+    for div in soup.find_all("a", {'class':'permalink'}):
+        div.decompose()
+
+    for item in soup.select("body"):
+        output = pypandoc.convert_text(soup.body, 'gfm', format='html')
+        message.send(output)
 
 if __name__ == "__main__":
     Bot().run()
