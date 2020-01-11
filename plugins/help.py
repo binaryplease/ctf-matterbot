fe4268e6c92d05acd56a0ec16beaf85229f47746
Constantin Kraft <constantin@websolutions.koeln>
2020-01-11 13:03:50 -0500

    Cosmetics: applied PEP8-compliant, opinionated formatting (black), removed stale imports, sorted them

--- plugins/help.py
+++ plugins/help.py
@@ -1,9 +1,9 @@
-from mmpy_bot.bot import listen_to
-from mmpy_bot.bot import respond_to
-
 import re
 
-@listen_to('!gimme help', re.IGNORECASE)
+from mmpy_bot.bot import listen_to
+
+
+@listen_to("!gimme help", re.IGNORECASE)
 def display_help(message):
 
     """
@@ -11,12 +11,13 @@ def display_help(message):
     commands. Add all new functionality here in the same way to the array.
     """
 
-    commands  =[
-            "`!gimme shell <language> [local IP] [port]` Give me a reverse-shell in <language>. Optionally specify [your IP] and [port]",
-            "`!gimme <cve-id>` Give me info about CVE-<id>",
-            "`!gimme tldr <tool>` Remind me how to use <tool>",
-            "`!gimme tool <tool>` Give me misuse of <tool>",
-            "`!gimme port <number>` Give me the service for <port>"]
+    commands = [
+        "`!gimme shell <language> [local IP] [port]` Give me a reverse-shell in <language>. Optionally specify [your IP] and [port]",
+        "`!gimme <cve-id>` Give me info about CVE-<id>",
+        "`!gimme tldr <tool>` Remind me how to use <tool>",
+        "`!gimme tool <tool>` Give me misuse of <tool>",
+        "`!gimme port <number>` Give me the service for <port>",
+    ]
 
     r = "I know the following commands:\n"
     for item in commands:
--- plugins/shell.py
+++ plugins/shell.py
@@ -1,11 +1,11 @@
-from mmpy_bot.bot import listen_to
-from mmpy_bot.bot import respond_to
-
-import re
 import os
+import re
 from string import Template
 
-@listen_to('!gimme shell (.*)', re.IGNORECASE)
+from mmpy_bot.bot import listen_to
+
+
+@listen_to("!gimme shell (.*)", re.IGNORECASE)
 def revshell(message, params):
 
     """
@@ -22,27 +22,28 @@ def revshell(message, params):
 
     # Set the optional parameters if specified and use defaults otherwise
     params = dict(
-            local_ip  = params_arr[1] if len(params_arr) > 1 else '127.0.0.1',
-            port      = params_arr[2] if len(params_arr) > 2 else '50000')
+        local_ip=params_arr[1] if len(params_arr) > 1 else "127.0.0.1",
+        port=params_arr[2] if len(params_arr) > 2 else "50000",
+    )
 
     # We first create whitelist of valid filenames. While we could skip this
     # check, it makes the application vultnerable to inserting arbitrary file
     # names like ../../../etc/passwd as a language
 
-    shells = os.listdir('./plugins/shells')
+    shells = os.listdir("./plugins/shells")
     if lang in shells:
         try:
-            with open('./plugins/shells/'+lang) as f:
-                message.send('Here is a reverse shell in `%s`' % lang)
+            with open("./plugins/shells/" + lang) as f:
+                message.send("Here is a reverse shell in `%s`" % lang)
 
                 # Render template with parameters and reply with the output
                 tmpl = Template(f.read())
                 rendered = tmpl.substitute(params)
-                message.send('```' + lang + '\n' + rendered + '```')
+                message.send("```" + lang + "\n" + rendered + "```")
         except IOError:
             pass
     else:
         # If an error occured, inform about available shells
         message.send("I don't know that language :(")
         message.send("Supported languages are:")
-        message.send(', '.join(shells))
+        message.send(", ".join(shells))
